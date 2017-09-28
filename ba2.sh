0a38507b2e12c85eccd9f08aecb39b57742ace2b
||kubur|| <31385163+mamoun-kubur@users.noreply.github.com>
2017-09-28 13:44:50 +0300

    Delete ba2.sh

--- ba2.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-exec git init
-exec git add -A
-exec git commit -m 'updated'
-exec git remote add origin https://github.com/mamoun-kubur/simple-linear-regression.git
-exec git push -u -f origin master
